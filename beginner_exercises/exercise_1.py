596b4296a293f21fd546f36d32f9f48a0e726085
Sseguya1988 <isseguya@gmail.com>
2023-08-28 18:57:55 +0300

    exercise 1

---
+++ beginner_exercises/exercise_1.py
@@ -0,0 +1,6 @@
+# Write a program to accept two numbers from the user and calculate multiplication
+num1 = int(input("Enter first number "))
+num2 = int(input("Enter second number "))
+
+res = num1 * num2
+print("Multiplication is", res)
