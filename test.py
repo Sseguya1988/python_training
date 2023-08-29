d1c3fb4fd29905c9ebc3b0bb1914695c5dac22b5
Sseguya1988 <isseguya@gmail.com>
2023-08-29 14:42:36 +0300

    Just trying out

--- test.py
+++ test.py
@@ -1,2 +1,15 @@
-# testing file commits
-# new additions
+# Getting started with python
+print("Hello WOrld")
+addition = 10+15
+print(addition)
+
+# Use of semicolons
+a = 5; b = 17
+sum = a + b
+print(sum)
+
+m = 36; h = 12; n = 20
+sum2 = m + n + h
+print(sum2)
+finalrslt = sum * sum2 - m; result = m/h * (a+n)
+print(finalrslt,result)
