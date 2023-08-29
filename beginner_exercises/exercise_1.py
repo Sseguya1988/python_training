0fe8934399e97dd07cba0c9ca2ffb533b41c611c
Sseguya1988 <isseguya@gmail.com>
2023-08-29 15:35:10 +0300

    beginners_exercise1

--- beginner_exercises/exercise_1.py
+++ beginner_exercises/exercise_1.py
@@ -1,6 +1,14 @@
 # Write a program to accept two numbers from the user and calculate multiplication
-num1 = int(input("Enter first number "))
-num2 = int(input("Enter second number "))
+#num1 = int(input("Enter first number "))
+#num2 = int(input("Enter second number "))
 
-res = num1 * num2
-print("Multiplication is", res)
+#res = num1 * num2
+#print("Multiplication is", res)
+
+# Calculate the multiplication and sum of two numbers
+numb1 = int(input("enter first number")); numb2 = int(input("enter second number"))
+if numb1 * numb2 <= 1000:
+    print("Product is", numb1*numb2)
+else:
+    print("Sum is", numb1+numb2)
+
