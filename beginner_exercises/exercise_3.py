2c6f291068a2176ac4d098a83c1b89bda03b1b06
Sseguya1988 <isseguya@gmail.com>
2023-09-04 10:18:22 +0300

    beginners_exercise1

--- beginner_exercises/exercise_3.py
+++ beginner_exercises/exercise_3.py
@@ -1,7 +1,10 @@
+
 # Input string
 word = input("Input a string: ")
 for x in range(len(word)):
     if x % 2 == 0:
-        print(word[x], end="")
+        print(word[x], end="\n")
     # Alternatively
 # print("Even characters: ", word[::2])
+
+
