4259ea3391514840a99d4532b8e7783d7232e9b1
Sseguya1988 <isseguya@gmail.com>
2023-09-06 11:09:06 +0300

    beginners_exercise1

---
+++ beginner_exercises/exercise_6.py
@@ -0,0 +1,4 @@
+# State the list of interest
+Given_list = [10, 20, 33, 46, 55]
+
+print(Given_list)
---
+++ pythonexercises_home/exercise1.py
@@ -0,0 +1,7 @@
+words = input("Enter a sentence: ").split()
+reverse_words = []
+for i in words:
+    reverse_words.append(i[::-1])
+new_sentence = " ".join(reverse_words)
+print(new_sentence)
+
