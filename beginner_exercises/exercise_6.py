b589b389ea0c2659b79090b4d3ddadb669e703ba
Sseguya1988 <isseguya@gmail.com>
2023-09-07 13:48:50 +0300

    beginners_exercise1

--- beginner_exercises/exercise_6.py
+++ beginner_exercises/exercise_6.py
@@ -1,5 +1,6 @@
 # Exercise 6: Display numbers divisible by 5 from a list
 # State the list of interest
 Given_list = [10, 20, 33, 46, 55]
-
-print(Given_list)
+for i in Given_list:
+    if i%5 == 0:
+        print(i)
--- pythonexercises_home/exercise_3.py
+++ pythonexercises_home/exercise_3.py
@@ -1,12 +1,24 @@
+lst = list(range(10,100,10))
+new_lst = []
+for i in lst:
+    if i%3 == 0:
+        new_lst.append(i)
+print(new_lst)
+
+
+"""
 # Remove values greater than 50 from a list
 
 number_list = list(range(10,100,10))
-print(number_list)
+# print(number_list)
 number_list1 = []
-for i in number_list:
+for i in number_list[:]:
     if i % 10 == 0:
         number_list1.append(i)
-    else:
+        print(number_list1)
+"""
+
+
 
 
 
