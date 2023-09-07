24817c31bcdfbc347177c2a4406c60920689c6ad
Sseguya1988 <isseguya@gmail.com>
2023-09-07 14:36:30 +0300

    beginners_exercise1

--- pythonexercises_home/exercise_3.py
+++ pythonexercises_home/exercise_3.py
@@ -1,6 +1,8 @@
 # Exercise 3: Remove items from a list while iterating
 lst = list(range(10,100,10))
+# Create an empty list
 new_lst = []
+# Use a for loop
 for i in lst:
     if i <= 50:
         new_lst.append(i)
