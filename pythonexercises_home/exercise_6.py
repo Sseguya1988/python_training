62a2f09e858fef8601622a79b8efe78254638eaa
Sseguya1988 <isseguya@gmail.com>
2023-09-09 14:04:17 +0300

    beginners_exercise1

--- pythonexercises_home/exercise_6.py
+++ pythonexercises_home/exercise_6.py
@@ -2,7 +2,7 @@
 di = {"A": 65, "B": 66, "C": 67, "D": 68, "E": 69, "F": 70}
 # List of keys to filter
 keys_filter = ["A", "C", "F"]
-# setup an empty dictionary
+# set up an empty dictionary
 filtered_dict = {}
 # for all keys in key_filter, if this key is in di, then this key and value is added to new dic
 for key in keys_filter:
