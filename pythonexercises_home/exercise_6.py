9edf13fdc6adc8442879e698d1dd08cd6617ebfb
Sseguya1988 <isseguya@gmail.com>
2023-09-09 14:03:58 +0300

    beginners_exercise1

---
+++ pythonexercises_home/exercise_6.py
@@ -0,0 +1,12 @@
+# Exercise 6: Filter dictionary to contain keys present in the given list
+di = {"A": 65, "B": 66, "C": 67, "D": 68, "E": 69, "F": 70}
+# List of keys to filter
+keys_filter = ["A", "C", "F"]
+# setup an empty dictionary
+filtered_dict = {}
+# for all keys in key_filter, if this key is in di, then this key and value is added to new dic
+for key in keys_filter:
+    if key in di:
+        filtered_dict[key] = di[key]
+print(filtered_dict)
+
