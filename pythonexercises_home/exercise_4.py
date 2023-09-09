77ea12e1031b57f99a1ea13a52ad593dadcd8fb5
Sseguya1988 <isseguya@gmail.com>
2023-09-09 12:15:40 +0300

    beginners_exercise1

--- pythonexercises_home/exercise_4.py
+++ pythonexercises_home/exercise_4.py
@@ -1,6 +1,9 @@
 # Exercise 4: Reverse Dictionary mapping
 ascii_dict = {"A": 65, "B": 66, "C": 67, "D": 68}
+# Create empty dictionary
 reverse_dict = {}
+# Use a for loop to re-assign elements in the set
 for key, value in ascii_dict.items():
-    reverse_dict[value]=key
+    reverse_dict[value] = key
+# print new dictionary
 print(reverse_dict)
