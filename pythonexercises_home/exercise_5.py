c54cab5eea379aa20a2007e2a1d6b53b93003a52
Sseguya1988 <isseguya@gmail.com>
2023-09-09 12:41:18 +0300

    beginners_exercise1

--- pythonexercises_home/exercise_5.py
+++ pythonexercises_home/exercise_5.py
@@ -1,2 +1,14 @@
 # Exercise 5: Display all duplicate items from a list
-sample_list = []
+sample_list = [10, 20, 60, 30, 20, 40, 30, 60, 70, 80]
+# Create empty lists for unique and duplicate numbers
+uniq = []
+dupl = []
+# use for loop to determine the duplicate values
+for i in sample_list:
+    if i not in uniq:
+        uniq.append(i)
+    else:
+        dupl.append(i)
+# Print duplicate values in a list
+print(dupl)
+
