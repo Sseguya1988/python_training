1ac91c2ef39c2ee1c828593e3d209957ba98671b
Sseguya1988 <isseguya@gmail.com>
2023-09-14 16:10:17 +0300

    beginners_exercise1

---
+++ Inputoutput_exercises/exercise1.py
@@ -0,0 +1,4 @@
+# Exercise 1: Accept numbers from a user
+x = int(input("Enter the first number: "))
+y = int(input("Enter the second number: "))
+print(f"Product of these numbers is: {x*y}")
--- pythonexercises_home/exercise_8.py
+++ pythonexercises_home/exercise_8.py
@@ -1,1 +1,2 @@
 # Exercise 8: Create an inner function
+def concat_function(x,y):
