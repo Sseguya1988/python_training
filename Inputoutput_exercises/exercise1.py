639b60d580c1e455d747b0096833687298e8344e
Sseguya1988 <isseguya@gmail.com>
2023-09-14 16:10:46 +0300

    beginners_exercise1

--- Inputoutput_exercises/exercise1.py
+++ Inputoutput_exercises/exercise1.py
@@ -1,4 +1,5 @@
 # Exercise 1: Accept numbers from a user
 x = int(input("Enter the first number: "))
 y = int(input("Enter the second number: "))
+# Print Product
 print(f"Product of these numbers is: {x*y}")
