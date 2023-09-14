9920c5f0cb37db30fc258c8072af032f293357bc
Sseguya1988 <isseguya@gmail.com>
2023-09-14 17:49:30 +0300

    beginners_exercise1

--- Inputoutput_exercises/exercise_4.py
+++ Inputoutput_exercises/exercise_4.py
@@ -1,7 +1,15 @@
 # Exercise 4: Display float number with 2 decimal places using print()
+num = input("Enter a float number: ")
+# convert to a float
+float_num = float(num)
+decimal_2 = "{:.2f}".format(float_num)
+print(decimal_2)
+"""
+# Alternative 1 (using direct string)
 float_num = 3.4527354
 decimal_2 = "{:.2f}".format(float_num)
 print(decimal_2)
+"""
 """
 # Alternative 1
 float_num = 1.234562
