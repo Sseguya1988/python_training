66e21b9be1c49e7208b13e944de29b0625a5b615
Sseguya1988 <isseguya@gmail.com>
2023-09-14 16:28:56 +0300

    beginners_exercise1

---
+++ Inputoutput_exercises/exercise_2.py
@@ -0,0 +1,5 @@
+# Exercise 2: Display three string “Name”, “Is”, “James” as “Name**Is**James”
+list1 = ["Name", "Is", "James"]
+# use join function to join the three strings
+list2 = "*".join(list1)
+print(list2)
