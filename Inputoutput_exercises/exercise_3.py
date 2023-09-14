c7724f9dede8e1c1bafd2bfc15400fd72cb02eee
Sseguya1988 <isseguya@gmail.com>
2023-09-14 16:47:00 +0300

    beginners_exercise1

---
+++ Inputoutput_exercises/exercise_3.py
@@ -0,0 +1,5 @@
+# Exercise 3: Convert Decimal number to octal using print() output formatting
+decimal_num = int(input("Enter the decimal number: "))
+# convert to octal
+oct_num = oct(decimal_num)
+print(oct_num)
