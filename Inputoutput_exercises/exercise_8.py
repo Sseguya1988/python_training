dd340381ac3b80f7a29022059d635d2561862d80
Sseguya1988 <isseguya@gmail.com>
2023-09-16 07:48:10 +0300

    beginners_exercise1

---
+++ Inputoutput_exercises/exercise_8.py
@@ -0,0 +1,5 @@
+# Exercise 8: Format variables using a string.format() method.
+total_money = input("totalMoney = ")
+quantity = input("quantity = ")
+price = float(input("price = "))
+print(f"I have {total_money} dollars so i can buy {quantity} football for {price} dollars.")
