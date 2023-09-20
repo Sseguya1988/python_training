c7b8dd8cbe5b8d8d669b7aab14336088ed40c4be
Sseguya1988 <isseguya@gmail.com>
2023-09-20 10:20:02 +0300

    beginners_exercise1

---
+++ loop_exercise/exercise_1.py
@@ -0,0 +1,11 @@
+# Exercise 1: Print First 10 natural numbers using while loop
+i = 1
+# use while loop
+while i <= 10:
+    print(i)
+    # increase i by one
+    i += 1
+"""
+for i in range(1,11):
+    print(i)
+"""
