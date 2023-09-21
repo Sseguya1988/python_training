12057b7bc7d2ca578eecfe81469005f0030ae1e2
Sseguya1988 <isseguya@gmail.com>
2023-09-21 09:17:43 +0300

    beginners_exercise1

---
+++ loop_exercise/exercise_6.py
@@ -0,0 +1,7 @@
+# Exercise 6: Count the total number of digits in a number
+num = int(input("Enter a number whose digita are required: "))
+# Convert number into a string
+num_str = str(num)
+length_num = len(num_str)
+# print the number of digits
+print(f"The number of digits in {num} is {length_num}")
