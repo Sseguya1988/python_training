f32fe335fc2ba6ee051f2ed428e359f145baa078
Sseguya1988 <isseguya@gmail.com>
2023-09-22 06:36:44 +0300

    beginners_exercise1

---
+++ loop_exercise/exercise_8.py
@@ -0,0 +1,8 @@
+# Exercise 8: Print list in reverse order using a loop
+list1 = [1, 2, 3, 4, 5, 6]
+# create an empty list to store these reversed items
+reversed_list = []
+# use a for loop ton reverse the items
+for i in reversed(list1):
+    reversed_list.append(i)
+print(reversed_list)
