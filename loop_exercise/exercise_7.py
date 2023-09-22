5e4bd215ac6462cdb87cc554fc25ba3c8bf95f96
Sseguya1988 <isseguya@gmail.com>
2023-09-22 07:04:33 +0300

    beginners_exercise1

--- loop_exercise/exercise_7.py
+++ loop_exercise/exercise_7.py
@@ -1,7 +1,16 @@
 # Exercise 7: Print the following pattern
+num_rows = 5
+# use nested loops to generate the
+for i in range(num_rows, 0, -1):
+    for j in range(i, 0, -1):
+        print(j, end=" ")
+    print()
+"""
 num_columns = 5
 # use nested loops to generate the
 for i in range(1, num_columns + 1):
     for j in range(i, 0, -1):
         print(j, end=" ")
     print()
+"""
+
