4d6cb2add21b9ee289e80eee26099fa73064b881
Sseguya1988 <isseguya@gmail.com>
2023-09-22 08:28:12 +0300

    beginners_exercise1

---
+++ loop_exercise/exercise_10.py
@@ -0,0 +1,15 @@
+# Exercise 10: Use else block to display a message “Done” after successful execution of for loop
+for number in range(1, 6):
+    if number <= 2:
+        print(f"{number} is less than or equal to 2")
+    else:
+        print(f"{number} is more than 2")
+else:
+    print("Done!")
+"""
+num = int(input("Enter a number of your choice: "))
+if num <= 2:
+    print(f"Number {num} entered is less or equal to 2 ")
+else:
+    print(f"Number {num} entered is greater than 2 ")
+"""
