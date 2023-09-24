3dc538a9286f5ae7b441274ca456a598cd465406
Sseguya1988 <isseguya@gmail.com>
2023-09-24 12:24:22 +0300

    beginners_exercise1

--- loop_exercise/exercise_16.py
+++ loop_exercise/exercise_16.py
@@ -3,4 +3,4 @@ number_n = int(input("Enter maximum number: "))
 # calculate and print the cube of numbers 1 to number_n
 for i in range(1, number_n + 1):
     cube = i**3
-    print(f"{i} cubed is {cube}")
+    print(f"Current number is: {i} and the cube is {cube}")
