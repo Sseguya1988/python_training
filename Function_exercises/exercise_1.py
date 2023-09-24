900155c2380a0f0e7f33e2be1dd9fc6490b8649a
Sseguya1988 <isseguya@gmail.com>
2023-09-24 16:19:12 +0300

    beginners_exercise1

---
+++ Function_exercises/exercise_1.py
@@ -0,0 +1,11 @@
+# Exercise 1: Create a function in Python
+# define a function
+def name_age(name, age):
+    print(f"Name: {name}")
+    print(f"Age: {age}")
+
+
+# input name and age
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+name_age(name, age)
