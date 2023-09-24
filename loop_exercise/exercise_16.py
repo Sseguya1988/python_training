2ee9ad37aa019cf650e27f3f2d84873a0d25761e
Sseguya1988 <isseguya@gmail.com>
2023-09-24 12:22:48 +0300

    beginners_exercise1

---
+++ loop_exercise/exercise_16.py
@@ -0,0 +1,6 @@
+# Exercise 16: Calculate the cube of all numbers from 1 to a given number
+number_n = int(input("Enter maximum number: "))
+# calculate and print the cube of numbers 1 to number_n
+for i in range(1, number_n + 1):
+    cube = i**3
+    print(f"{i} cubed is {cube}")
