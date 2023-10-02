2156bdb696cacb04b22df597b1e1107503471036
Sseguya1988 <isseguya@gmail.com>
2023-10-02 09:43:55 +0300

    beginners_exercise1

--- Data_structure_exercise/exercise_1.py
+++ Data_structure_exercise/exercise_1.py
@@ -1,11 +1,11 @@
 # Exercise 1: Create a list by picking an odd-index items from the first list and even index items from the second
 # Generate sample lists
-l1 = [1, 2, 3, 4, 5, 6]
-l2 = ["a", "b", "c", "d", "e", "f"]
+l1 = [3, 6, 9, 12, 15, 18, 21]
+l2 = [4, 8, 12, 16, 20, 24, 28]
 # define an empty list l3
 l3 = []
 # iterate elements of l1 and l2 using zip into l3
-for item1, item2 in zip(l1[1::2], l2[::2]):
+for item1, item2 in zip(l1[1::2], l2[2::2]):
     l3.extend([item1, item2])
 
 print(f"The new list is {l3}")
