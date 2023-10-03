ac70e6d655dec5e422d8badcaddcd00073a1b575
Sseguya1988 <isseguya@gmail.com>
2023-10-03 11:39:29 +0300

    beginners_exercise1

---
+++ list_exercise/exercise_4.py
@@ -0,0 +1,5 @@
+# Exercise 4: Concatenate two lists in the following order
+list1 = ["Hello ", "take "]
+list2 = ["Dear", "Sir"]
+concat_list = [x + y for x in list1 for y in list2]
+print(concat_list)
