3774ab5612b78236990c85ebc9832239e3c73dcc
Sseguya1988 <isseguya@gmail.com>
2023-10-03 12:32:17 +0300

    beginners_exercise1

---
+++ list_exercise/exercise_8.py
@@ -0,0 +1,8 @@
+# Exercise 8: Extend nested list by adding the sublist
+list1 = ["a", "b", ["c", ["d", "e", ["f", "g"], "k"], "l"], "m", "n"]
+
+# sub list to add
+sub_list = ["h", "i", "j"]
+# To extend list containing "f" and "g"
+list1[2][1][2].extend(sub_list)
+print(list1)
