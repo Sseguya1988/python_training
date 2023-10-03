1ea4a9be6eb3cb8db82fc10f9b4a954b0c564526
Sseguya1988 <isseguya@gmail.com>
2023-10-03 17:08:15 +0300

    beginners_exercise1

---
+++ set_exercises/exercise_6.py
@@ -0,0 +1,5 @@
+# Exercise 6: Return a set of elements present in Set A or B, but not both
+set1 = {10, 20, 30, 40, 50}
+set2 = {30, 40, 50, 60, 70}
+set1_union_set2 = set1 ^ set2
+print(set1_union_set2)
