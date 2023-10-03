fe8d1f5ff9b09a70a558be2aa8af6f0dc0442591
Sseguya1988 <isseguya@gmail.com>
2023-10-03 16:40:53 +0300

    beginners_exercise1

---
+++ set_exercises/exercise_3.py
@@ -0,0 +1,6 @@
+# Exercise 3: Get Only unique items from two sets
+set1 = {10, 20, 30, 40, 50}
+set2 = {30, 40, 50, 60, 70}
+# combine the two sets removing duplicates
+new_set = set1 | set2
+print(new_set)
