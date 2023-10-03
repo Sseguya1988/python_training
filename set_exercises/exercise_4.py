8f4c5b586006db6b60ad68b114aacaaae7b2abbb
Sseguya1988 <isseguya@gmail.com>
2023-10-03 16:45:32 +0300

    beginners_exercise1

---
+++ set_exercises/exercise_4.py
@@ -0,0 +1,6 @@
+# Exercise 4: Update the first set with items that don’t exist in the second set
+set1 = {10, 20, 30}
+set2 = {20, 40, 50}
+# compute the difference set1 - set2
+difference_sets = set1 - set2
+print(difference_sets)
