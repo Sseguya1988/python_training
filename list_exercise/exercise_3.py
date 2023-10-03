e9840a3973b3164da51d7ce20aa9283164160e6a
Sseguya1988 <isseguya@gmail.com>
2023-10-03 11:27:34 +0300

    beginners_exercise1

---
+++ list_exercise/exercise_3.py
@@ -0,0 +1,6 @@
+# Exercise 3: Turn every item of a list into its square
+numbers = [1, 2, 3, 4, 5, 6, 7]
+# square the numbers in list
+num_squared = [i**2 for i in numbers]
+print(f"List {num_squared} is a list of squared numbers from list {numbers}")
+
