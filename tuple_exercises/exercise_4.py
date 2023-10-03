64ec7e8441a8235d36270aa432fc348ca850d1e8
Sseguya1988 <isseguya@gmail.com>
2023-10-03 17:54:51 +0300

    beginners_exercise1

---
+++ tuple_exercises/exercise_4.py
@@ -0,0 +1,10 @@
+# Exercise 4: Unpack the tuple into 4 variables
+tuple1 = (10, 20, 30, 40)
+# unpack this tuple int variables
+a, b, c, d = tuple1
+print(f"tuple1 = {tuple1}")
+print("# Your code")
+print(f"print(a) # should print {a}")
+print(f"print(b) # should print {b}")
+print(f"print(c) # should print {c}")
+print(f"print(d) # should print {d}")
