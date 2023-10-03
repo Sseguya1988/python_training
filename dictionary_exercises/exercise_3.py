8e467ef01450f0dd4e31a731588ee13ddb26eb9a
Sseguya1988 <isseguya@gmail.com>
2023-10-03 13:07:55 +0300

    beginners_exercise1

---
+++ dictionary_exercises/exercise_3.py
@@ -0,0 +1,14 @@
+# Exercise 3: Print the value of key ‘history’ from the below
+sampleDict = {
+    "class": {
+        "student": {
+            "name": "Mike",
+            "marks": {
+                "physics": 70,
+                "history": 80
+            }
+        }
+    }
+}
+value_history = sampleDict["class"]["student"]["marks"]["history"]
+print(value_history)
