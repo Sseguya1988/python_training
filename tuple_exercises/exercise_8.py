d8f9023945ba46f2cd7788b309ff208c26fa066e
Sseguya1988 <isseguya@gmail.com>
2023-10-03 18:24:31 +0300

    beginners_exercise1

--- tuple_exercises/exercise_8.py
+++ tuple_exercises/exercise_8.py
@@ -1,5 +1,5 @@
 # Exercise 8: Sort a tuple of tuples by 2nd item
 tuple1 = (('a', 23), ('b', 37), ('c', 11), ('d', 29))
 # sort the tuple
-sorted_tuple = sorted(tuple1, key = lambda i: i[1])
+sorted_tuple = sorted(tuple1, key=lambda i: i[1])
 print(sorted_tuple)
