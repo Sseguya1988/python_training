9b046bfa26e63ed3f0429fccc43695b3827350bd
Sseguya1988 <isseguya@gmail.com>
2023-10-03 16:36:09 +0300

    beginners_exercise1

---
+++ set_exercises/exercise_2.py
@@ -0,0 +1,6 @@
+# Exercise 2: Return a new set of identical items from two sets
+set1 = {10, 20, 30, 40, 50}
+set2 = {30, 40, 50, 60, 70}
+# find set of intersection for set1 and set2
+intersection_set = set1.intersection(set2)
+print(intersection_set)
