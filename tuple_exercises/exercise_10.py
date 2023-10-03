455218e81f313e198991f2ebf47d7e3c86171f50
Sseguya1988 <isseguya@gmail.com>
2023-10-03 18:39:23 +0300

    beginners_exercise1

---
+++ tuple_exercises/exercise_10.py
@@ -0,0 +1,7 @@
+# Exercise 10: Check if all items in the tuple are the same
+tuple1 = (45, 45, 45, 45)
+# set condition to test the tuple
+if len(set(tuple1)) == 1:
+    print("True")
+else:
+    print("False")
