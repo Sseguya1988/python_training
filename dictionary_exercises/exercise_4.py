6dedf654f5a42ac75f3be26aa467256e2ae99d98
Sseguya1988 <isseguya@gmail.com>
2023-10-03 13:17:37 +0300

    beginners_exercise1

--- dictionary_exercises/exercise_4.py
+++ dictionary_exercises/exercise_4.py
@@ -1,5 +1,6 @@
 # Exercise 4: Initialize dictionary with default values
 employees = ['Kelly', 'Emma']
 defaults = {"designation": 'Developer', "salary": 8000}
+# define new dictionary
 new_dict = {keys: defaults for keys in employees}
 print(new_dict)
