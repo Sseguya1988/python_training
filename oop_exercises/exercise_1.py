cfa93e36beac2e3be1e442d6d73969979c02b4a3
Sseguya1988 <isseguya@gmail.com>
2023-10-05 15:59:19 +0300

    beginners_exercise1

--- oop_exercises/exercise_1.py
+++ oop_exercises/exercise_1.py
@@ -1,13 +1,13 @@
 # Exercise 1: Create a Class with instance attributes
-class students:
+class Students:
     def __init__(self, marks, grade):
         self.marks = marks
         self.grade = grade
 
 
 # create instances for marks
-student1 = students(56, "C-")
-student2 = students(72, "B+")
+student1 = Students(56, "C-")
+student2 = Students(72, "B+")
 # access and print attributes of the instances
 print(f"Student1 marks obtained: {student1.marks} and the grade is {student1.grade}")
 print(f"Student2 marks obtained: {student2.marks} and the grade is {student2.grade}")
