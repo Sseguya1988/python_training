b1c29e9896db8439a43dc4cb28979daf8d8a7ab4
Sseguya1988 <isseguya@gmail.com>
2023-10-06 11:37:35 +0300

    beginners_exercise1

---
+++ json_exercise/exercise_1.py
@@ -0,0 +1,6 @@
+# Exercise 1: Convert the following dictionary into JSON format
+import json
+# Given dictionary
+data = {"key1": "value1", "key2": "value2"}
+# convert dictionary to a json
+print(f"data = {json.dumps(data)}")
