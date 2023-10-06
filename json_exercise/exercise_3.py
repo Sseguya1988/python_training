9532022e91179dd7330be3e52f8a1324d5c2b3a6
Sseguya1988 <isseguya@gmail.com>
2023-10-06 13:41:45 +0300

    beginners_exercise1

--- json_exercise/exercise_3.py
+++ json_exercise/exercise_3.py
@@ -1,2 +1,11 @@
 # Exercise 3: PrettyPrint following JSON data
-
+import json
+# sample json
+sampleJson = {
+    "key1": "value1",
+    "key2": "value2",
+    "key3": "value3"
+}
+# prettyprint following json
+new_json_sample = json.dumps(sampleJson, indent=2, separators=(",", "="))
+print(new_json_sample)
