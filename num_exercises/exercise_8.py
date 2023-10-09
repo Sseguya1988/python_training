cdb0567b7b3960c7d4a25a18b30341800ddc1158
Sseguya1988 <isseguya@gmail.com>
2023-10-09 22:42:49 +0300

    beginners_exercise1

--- num_exercises/exercise_8.py
+++ num_exercises/exercise_8.py
@@ -1,15 +1,12 @@
+# Exercise 8: Print max from axis 0 and min from axis 1 from the following 2-D array.
 import numpy as np
 
-# Create a NumPy array
+# state given array
 sampleArray = np.array([[34, 43, 73], [82, 22, 12], [53, 94, 66]])
-
-# Get the sorted indices based on the second column (column index 1)
-sorted_indices = np.argsort(sampleArray[:, 1])
-
-# Use the sorted indices to reorder the rows of the original array
-sorted_array = sampleArray[sorted_indices]
-
-# Print the sorted array
-print(sampleArray)
-print("Sorted array based on the second column:")
-print(sorted_array)
+# max from x-axis
+max_from_x_axis = np.max(sampleArray, axis=0)
+# min from axis =1
+min_from_axia_1 = np.min(sampleArray, axis=1)
+# print max from x axis
+print(f" The maximum from axis 0 is {max_from_x_axis}")
+print(f"The minimum from axis 1 is {min_from_axia_1}")
