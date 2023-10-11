6dedaf21e6334e0928c453c76c2444e0dc65447d
Sseguya1988 <isseguya@gmail.com>
2023-10-11 17:26:00 +0300

    beginners_exercise1

---
+++ matplotlib_exercises/exercise_2.py
@@ -0,0 +1,23 @@
+# Exercise 2: Get total profit of all months and show line plot with the following Style properties
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load the csv data file
+sales_data = pd.read_csv("company_sales_data.csv")
+
+# extract "Months" and "Total Profits" data
+months = sales_data["month_number"]
+profits = sales_data["total_profit"]
+
+# generate plot for the total profits over months
+plt.plot(months, profits, marker="o", linestyle="dotted", color="r", linewidth="3", markersize="6", label="Total Profits")
+
+# set X and Y labels
+plt.xlabel("Month Number")
+plt.ylabel("Solid units number")
+
+# add a legend
+plt.legend(loc="lower right")
+
+# plot this graph
+plt.show()
