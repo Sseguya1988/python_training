5ffb57cbd0ad64fbfb5c93d4a7c19ca567ffe11b
Sseguya1988 <isseguya@gmail.com>
2023-10-11 18:50:39 +0300

    beginners_exercise1

--- matplotlib_exercises/exercise_4.py
+++ matplotlib_exercises/exercise_4.py
@@ -10,7 +10,7 @@ months = sales_data["month_number"]
 tooth_paste = sales_data["toothpaste"]
 
 # generate a scatter plot
-plt.scatter(months, tooth_paste, color="b", marksize="6", label="Tooth paste Sales data")
+plt.scatter(months, tooth_paste, color="b", label="Tooth paste Sales data")
 
 # set X and Y axis label
 plt.xlabel("Month Number")
@@ -19,8 +19,11 @@ plt.ylabel("Number of units Sold")
 # set title
 plt.title("Tooth paste Sales data")
 
+# add a grid
+plt.grid(True, linestyle="dashed", color="gray", linewidth="0.5")
 
 # set legend
 plt.legend(loc="upper left")
 
-# set the
+# show the plot
+plt.show()
