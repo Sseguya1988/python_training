d620163ecbd05198c0916143d01c7f4379608adb
Sseguya1988 <isseguya@gmail.com>
2023-10-13 15:04:47 +0300

    beginners_exercise1

--- pythonexercises_home/exercise_8.py
+++ pythonexercises_home/exercise_8.py
@@ -1,2 +1,16 @@
 # Exercise 8: Create an inner function
-def concat_function(x,y):
+def out_function(str1, str2):
+    def new_concat(string, str3):
+        return string + str3
+
+    concat_string = str1 + str2
+    str3 = "Developers"
+    result = new_concat(concat_string, str3)
+    return result
+
+
+# apply this function
+string_one = input("Enter the first string: ")
+string_two = input("Enter the second string: ")
+final_result = out_function(string_one, string_two)
+print(final_result)
