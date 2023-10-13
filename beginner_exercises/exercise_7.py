626a49427120f216632fa47aad24ed883c43e0f5
Sseguya1988 <isseguya@gmail.com>
2023-10-13 15:35:00 +0300

    beginners_exercise1

---
+++ beginner_exercises/exercise_7.py
@@ -0,0 +1,11 @@
+# Exercise 7: Return the count of a given substring from a string
+
+# state the given string
+str_x = "Emma is good developer. Emma is a writer"
+
+# set the substring to count
+substring = "Emma"
+
+# count how many times this exists
+Emma_count = str_x.count(substring)
+print(Emma_count)
