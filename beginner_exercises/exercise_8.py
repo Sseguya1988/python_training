8baa9195c49b09c1fdf6c73a22cb42eaacf7e80d
Sseguya1988 <isseguya@gmail.com>
2023-10-13 16:45:52 +0300

    beginners_exercise1

--- beginner_exercises/exercise_8.py
+++ beginner_exercises/exercise_8.py
@@ -1,1 +1,8 @@
-#
+# Exercise 8: Print the following pattern
+num_rows = int(input("Enter the number of rows: "))
+
+# use a nested for loop to draw required triangle
+for i in range(1, num_rows +1):
+    for j in range(1, i +1):   # inner loop that prints number for each row
+        print(i, end=" ")
+    print()
